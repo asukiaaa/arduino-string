144dc2b6d956eac0210de5c441441dceb4bc98bb
Asuki Kono <asukiaaa@gmail.com>
2023-12-05 10:05:45 +0900

    Remove indent in namespace

--- src/string_asukiaaa.h
+++ src/string_asukiaaa.h
@@ -8,18 +8,18 @@
 #define STRING_ASUKIAAA_VERSION_PATCH 3
 
 namespace string_asukiaaa {
-  String padStart(const String& str, unsigned int len, char pad);
-  String padNumStart(int num, unsigned int len, char pad);
-  String padNumStart(long num, unsigned int len, char pad);
-  String padNumStart(float num, unsigned int len, char pad);
-  String padNumStart(double num, unsigned int len, char pad);
-  String padEnd(const String& str, unsigned int len, char pad);
-  String padNumEnd(int num, unsigned int len, char pad);
-  String padNumEnd(long num, unsigned int len, char pad);
-  String padNumEnd(float num, unsigned int len, char pad);
-  String padNumEnd(double num, unsigned int len, char pad);
-  String yesNo(bool value);
-  String trueFalse(bool value);
-}
+String padStart(const String& str, unsigned int len, char pad);
+String padNumStart(int num, unsigned int len, char pad);
+String padNumStart(long num, unsigned int len, char pad);
+String padNumStart(float num, unsigned int len, char pad);
+String padNumStart(double num, unsigned int len, char pad);
+String padEnd(const String& str, unsigned int len, char pad);
+String padNumEnd(int num, unsigned int len, char pad);
+String padNumEnd(long num, unsigned int len, char pad);
+String padNumEnd(float num, unsigned int len, char pad);
+String padNumEnd(double num, unsigned int len, char pad);
+String yesNo(bool value);
+String trueFalse(bool value);
+}  // namespace string_asukiaaa
 
 #endif
